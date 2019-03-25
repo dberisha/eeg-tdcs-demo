3aaf26af15cc01fcf8ba64aca25e0f24eb86273c
Destiny Berisha <destinyberisha@Destinys-MacBook-Pro-3.local>
2019-03-25 15:11:48 -0400

    Finished prepping Data.

--- prepData.m
+++ prepData.m
@@ -9,9 +9,19 @@
 
 % - DTN: 3/22/2019 (Destiny)
 
+
+% Notes by Destiny:
+
+% First we are loading in the streams, and extracting the MARKERS from the
+% DAQ. Then we are manually saving (and checking) the blocks that are
+% aligned with these MARKERS. Then we are extracting the tiemstamps from
+% the EEG data, and matching those up. Then matching up the blocks of the
+% EEG timestamps to the actual EEG data. 
+
+% Remember the timestamps are coming from the universal clock. 
+
 %% Clearing all variables and setting directories
 
-ccc;
 
 inDir = '/Users/destinyberisha/Desktop/eeg-tdcs-demo/data/raw/xdf';
 outDir = '/Users/destinyberisha/Desktop/eeg-tdcs-demo/data/processed/epochedMat';
@@ -80,10 +90,92 @@ plot(markers_time, trigger, '.');
 [ends,~] = ginput(7);
 
 close all;
+%% Calculate the epoch - segmenting trigger info for trial blocks
+
+segEndInd=zeros(1,7);
+segEngTime=zeros(1,7);
+epochedTriggerCell = {};
+
+for k=1:length(starts)
+    b=1;
+    for i=1:length(markers_time)
+        if markers_time(i)>=starts(k) && markers_time(i)<ends(k)
+        epochedTriggerCell{k}(b)= markers_time(i);
+        segEngTime(k)=markers_time(i);
+        segEndInd(k)=i;
+        b=b+1;
+        end
+    end
+end
 
 
+ %% Sanity Check
+ 
+ for i=1:length(epochedTriggerCell)
+     A(i)=size(epochedTriggerCell{i},2);
+ end
+ 
+ prompt=sprintf('\nAre the num of the sizes of the segmented chunks (%d) equal to the original size (%d)? (y/n). \n', sum(A), size(markers_time,2));
+ check = input(prompt, 's');
+ 
+ if strcmp(check,'y')
+     fprintf('\nYou''re good to go.')
+ else
+     fprintf('\nYou probably marked off data segments incorrectly in the figure, go back and try again.\n')
+ end
+ 
+%% Epoching EEG based on segmented trial blocks
 
 
+epochedEEGCell={};
+epochedEEGTimeCell={};
+sFlashTime=1; % adding 1 second to the final cut of the trial block to be safe
+
+for i=1:length(epochedTriggerCell)
+    sTime=epochedTriggerCell{i}(1);
+    eTime=epochedTriggerCell{i}(end) + sFlashTime;
+    [~,sInd]=min(abs(eeg_time-sTime));
+    [~,eInd]=min(abs(eeg_time-eTime));
+    
+    epochedEEGCell{i}=eeg(sInd:eInd,:);
+    epochedEEGTimeCell{i}=eeg_time(sInd:eInd);
+end
+
+%% Splitting up the data and saving it out
+
+subID='S01';
+condStr='Anodal';
+
+fprintf('\n Saving...Please be patient. \n');
+
+cd(outDir)
+
+for i=1:7
+    
+    epochedEEG = epochedEEGCell{i};
+    epochedTrigger = epochedTriggerCell{i};
+    epochedEEGTime =epochedEEGTimeCell{i};
+    
+    if i<=2
+        endTag = '_preStim';
+        bTag = sprintf('_b%02d.mat',i);
+    elseif i>=3 && i<=5
+        endTag = '_durStim';
+        bTag = sprintf('_b%02d.mat',i);
+    elseif i>=6
+        endTag = '_posStim';
+        bTag = sprintf('_b%02d.mat',i);
+    end
+    
+    epochedFilename=[subID condStr endTag bTag];
+    save(epochedFilename, 'epochedEEG', 'epochedTrigger','epochedEEGTime','fs_eeg', 'subID', 'condStr');
+    clear epochedEEG epochedTrigger epochedEEGTime
+end
+
+% -7.3v
+% add this tag if you need to save out more than 2GB
+
+fprintf('\n Save Complete.\n')
 
 
 
